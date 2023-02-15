77114f363c5c495aab153934ac8d89e5ad1ed15f
sajim-khan <shossain1427@gmail.com>
2023-02-16 02:47:08 +0600

    added withdraw section by using function

--- js/deposit.js
+++ js/deposit.js
@@ -1,5 +1,4 @@
 
-
 document.getElementById("btn-deposit").addEventListener("click", function() {
     const newDepositAmount = getInputFieldValueById('deposit-field');
     const previousDepositTotal = getTextElementValueById('deposit-total');
