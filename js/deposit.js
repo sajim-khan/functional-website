77b27f262e4feb7fe6064b4a7c086e01e7939409
sajim-khan <shossain1427@gmail.com>
2023-02-16 00:15:33 +0600

    added deposit functional task

--- js/deposit.js
+++ js/deposit.js
@@ -1,1 +1,17 @@
-console.log('Running')
+function getInputValueById(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputFieldValueString = inputField.value;
+    inputField.value = '';
+    return inputFieldValueString;
+}
+
+function getElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueString = element.innerText;
+    const value = parseFloat(elementValueString);
+    return value;
+}
+
+document.getElementById("btn-deposit").addEventListener("click", function() {
+    const newDepositAmount = getInputValueById("deposit-field");
+})
